3f84085633dbbd31aad162b2115903e8763e3d8f
Jacob Hands <jacob@gogit.io>
2020-08-31 15:38:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+filters = [
+  ['col0', 5],
+  ['col1', 7]
+]
+data = [
+  # col0, col1
+  [1, 3],
+  [5, 9],
+  [2, 7]
+]
+colMap = {
+  'col0': 0,
+  'col1': 1
+}
+results = []
+for d in data:
+  for f in filters:
+    filterColName = f[0]
+    filterColValue = f[1]
+    if(d[colMap[filterColName]] == filterColValue):
+      results += [d]
+      break # A row can match multiple filters, only match once.
+
+print(results)
